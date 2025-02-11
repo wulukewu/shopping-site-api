c71b29dbd225704e50e95f9e4544ab366edb9c12
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-02-11 17:44:42 +0800

    feat: connect to mongoose

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs'); // Import the file system module
+const fs = require('fs');
+const path = require('path');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
-// Load products from products.json (same location as your Vue app for now)
+// MongoDB Connection
+const mongoose_url =
+  'mongodb+srv://dbUser:6PVLqEdkaKiNBgTe@shopping-site.mch4s.mongodb.net/?retryWrites=true&w=majority&appName=shopping-site';
+mongoose
+  .connect(mongoose_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+// Load products from products.json (temporary - will be replaced by database)
 const products = JSON.parse(
   fs.readFileSync('../shopping-site/public/products.json', 'utf-8')
 );
 
-app.get('/', (req, res) => {
-  res.send('Hello from the Shopping Site API!');
-});
-
 app.get('/products', (req, res) => {
-  res.json(products); // Send the products as JSON
+  res.json(products);
 });
 
 app.listen(port, () => {
