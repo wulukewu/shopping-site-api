be34b8459df11603a854a36a354390522b23cf40
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-02-12 11:06:56 +0800

    fix: npm test with mocha

---
+++ test.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+describe('Application Functionality', () => {
+    it('should build the application successfully', () => {
+        assert.strictEqual(true, true);
+    });
+});
